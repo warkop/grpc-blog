90c3377e508547f97adc42158016529761b24f3a
Mohammad Khafid Barokum <khafid@majoo.id>
2021-12-09 22:33:07 +0700

    client read blog

--- blog/blog_client/client.go
+++ blog/blog_client/client.go
@@ -26,9 +26,26 @@ func main() {
 		Title:    "my book",
 		Content:  "konten orang",
 	}
-	createBlog, err := c.CreateBlog(context.Background(), &blogpb.CreateBlogRequest{Blog: blog})
+	createBlogRes, err := c.CreateBlog(context.Background(), &blogpb.CreateBlogRequest{Blog: blog})
 	if err != nil {
 		log.Fatalf("Unexpected error: %v", err)
 	}
-	fmt.Printf("createblog: %v", createBlog)
+
+	blogId := createBlogRes.GetBlog().GetId()
+
+	// read blog
+	_, err2 := c.ReadBlog(context.Background(), &blogpb.ReadBlogRequest{
+		BlogId: "61b2119e12ae320038b78d12",
+	})
+	if err2 != nil {
+		fmt.Printf("error happened while reading: %v", err2)
+	}
+
+	readBlogReq := &blogpb.ReadBlogRequest{BlogId: blogId}
+	readBlogRes, readBlogErr := c.ReadBlog(context.Background(), readBlogReq)
+	if readBlogErr != nil {
+		fmt.Printf("error happened while reading: %v", readBlogErr)
+	}
+
+	fmt.Printf("blog was read: %v", readBlogRes)
 }
